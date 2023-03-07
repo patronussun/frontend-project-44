47bbd7090e545fc0b1fef0360e54d75d39985c71
Maria Vasileva <mariia.vasileva@dualbootpartners.com>
2023-03-07 11:17:23 +0300

    setup eslint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greetingUser = () => {
+const greetingUser = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-}
+};
+
+export default greetingUser;
