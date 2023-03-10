a8d7321ff83dba4d788e51f650748d040f9e3462
Maria Vasileva <mariia.vasileva@dualbootpartners.com>
2023-03-10 10:48:15 +0300

    move common func to index.js

--- src/even-game.js
+++ src/even-game.js
@@ -1,42 +1,21 @@
-import readlineSync from 'readline-sync';
+import {
+  generateRandomNumber, getAnswer, checkIsWin, askQuestion, isCorrect,
+} from './index.js';
 
-const getAnswer = (text) => {
-  const answer = readlineSync.question(text);
-  return answer;
-};
-
-const generateRandomNumber = (max = 100) => Math.floor(Math.random() * max);
-
-const isCorrect = (number, answer, count, userName) => {
-  const result = count;
-  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
-  if (answer === correctAnswer) {
-    console.log('Correct!');
-    return result + 1;
-  }
-
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${userName}!`);
-  return false;
-};
-
-const checkIsWin = (count, userName) => {
-  if (count === 3) {
-    console.log(`Congratulations, ${userName}!`);
-  }
-};
+const checkIsEven = (number) => (number % 2 === 0 ? 'yes' : 'no');
 
 const evenGame = (userName) => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  askQuestion('Answer "yes" if the number is even, otherwise answer "no".');
   let count = 0;
 
   while (count < 3 && count !== false) {
     const randomNumber = generateRandomNumber();
     console.log(`Question: ${randomNumber}`);
 
+    const correctAnswer = checkIsEven(randomNumber);
     const answer = getAnswer('Your answer: ');
-    count = isCorrect(randomNumber, answer, count, userName);
+    count = isCorrect(correctAnswer, answer, count, userName);
   }
-
   checkIsWin(count, userName);
 };
 
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import readlineSync from 'readline-sync';
+
+const getAnswer = (text) => {
+  const answer = readlineSync.question(text);
+  return answer;
+};
+
+const incorrectAnswer = (answer, correctAnswer, userName) => {
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${userName}!`);
+};
+
+const generateRandomNumber = (max = 100) => Math.floor(Math.random() * max);
+
+const checkIsWin = (count, userName) => {
+  if (count === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
+
+const askQuestion = (question) => {
+  console.log(question);
+};
+
+const isCorrect = (correctAnswer, answer, count, userName) => {
+  const result = count;
+  if (answer === correctAnswer) {
+    console.log('Correct!');
+    return result + 1;
+  }
+
+  incorrectAnswer(answer, correctAnswer, userName);
+  return false;
+};
+
+export {
+  generateRandomNumber, getAnswer, checkIsWin, askQuestion, isCorrect,
+};
