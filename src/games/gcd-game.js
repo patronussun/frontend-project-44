4a9348702a9f4319aa77a67099eed9b3b330e015
Maria Vasileva <mariia.vasileva@dualbootpartners.com>
2023-03-20 20:32:43 +0300

    fix gcd game

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -1,29 +1,23 @@
-import readlineSync from 'readline-sync';
-import greetingUser from '../cli.js';
-
-import {
-  checkIsWin,
-  isCorrect,
-} from '../index.js';
-
+import runGame from '../index.js';
 import getRandomInt from '../utils.js';
 
-const DEFAULT_RESULT = 1;
+const DEFAULT_RESULT = '1';
 
 const generateNumbers = () => {
   const firstNumber = getRandomInt(1, 100);
   const secondNumber = getRandomInt(1, 100);
-  return [firstNumber, secondNumber];
+  return [firstNumber, secondNumber].join(' ');
 };
 
-const calculateResult = ([first, second]) => {
-  const isFirstBigger = first > second;
-  const smallNumber = isFirstBigger ? second : first;
-  const bigNumber = isFirstBigger ? first : second;
+const calculateResult = (numbers) => {
+  const [first, second] = numbers.split(' ');
+  const isFirstBigger = Number(first) > Number(second);
+  const smallNumber = isFirstBigger ? Number(second) : Number(first);
+  const bigNumber = isFirstBigger ? Number(first) : Number(second);
 
   for (let i = smallNumber; i > 0; i -= 1) {
     if ((bigNumber % i === 0) && (smallNumber % i === 0)) {
-      return i;
+      return i.toString();
     }
   }
 
@@ -31,18 +25,8 @@ const calculateResult = ([first, second]) => {
 };
 
 const gcdGame = () => {
-  console.log('Find the greatest common divisor of given numbers.');
-  let count = 0;
-
-  while (count < 3 && count !== false) {
-    const numbers = generateNumbers();
-    console.log(`Question: ${numbers[0]} ${numbers[1]}`);
-
-    const correctAnswer = calculateResult(numbers);
-    const answer = Number(readlineSync.question('Your answer: '));
-    count = isCorrect(correctAnswer, answer, count, userName);
-  }
-  checkIsWin(count, userName);
+  const gameDescription = 'Find the greatest common divisor of given numbers.';
+  runGame(gameDescription, generateNumbers, calculateResult);
 };
 
 export default gcdGame;
