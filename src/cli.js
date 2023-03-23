3704bc9d32c2e5eb9596bad5a04467c8759205fd
Maria Vasileva <mariia.vasileva@dualbootpartners.com>
2023-03-23 18:12:16 +0300

    update index file: move check wins func to main func, remove magic numbers; remove unued files

--- src/cli.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const greetingUser = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  return userName;
-};
-
-export default greetingUser;
